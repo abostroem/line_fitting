471480358bde857c732389be6a5fd58ed80bf344
Azalee Bostroem <abostroem@gmail.com>
2018-06-05 16:02:06 -0700

    Updated example of interactive fitting to match refactor

--- example/interactive_line_fitting.py
+++ example/interactive_line_fitting.py
@@ -1,10 +1,11 @@
 import os
 
-from fit_spectral_lines import fit_feature
+from fit_spectral_lines import define_feature
 import line_analysis_BSNIP
 
-for iline in line_list.keys():
-            spectrum = line_analysis_BSNIP.read_iraf_spectrum(filename)
-            min_list, pew_list, fig = fit_feature(spectrum, iline, binsize, absorption=True, 
-            similar_widths=True, fixed_offset=True, offsets = [44, 164],
-            input_filename=os.path.join(OUTPUT_DIR, '{}_input.yaml'.format(iline)))
+iline = 'CaII'
+
+min_list, pew_list, fig = define_feature(spectrum, iline, binsize, absorption=True, 
+                                                     similar_widths=True, fixed_offset=True, offsets = [44, 164],
+                                                     input_filename='{}_input.yaml'.format(iline), 
+                                                     input_append = append, interactive=True)
