2bb14d49427e69c3cc8cffbed201bf36994b6567
Azalee Bostroem <abostroem@gmail.com>
2018-06-22 17:14:10 -0700

    Added keyword to define_fit that defines the fit range to be the same as the continuum
    Added documentation for define_feature and fit_feature
    Added documentation for define_featuer and fit_feature to README

--- fit_spectral_lines.py
+++ fit_spectral_lines.py
@@ -149,6 +149,54 @@ def fit_feature(line_wave, line_flux, fit_wave, fit_type, center_list, ax1, ax2,
                 continuum_l, continuum_r,
                 offsets=None, fixed_offset=False, similar_widths=True, absorption=True,
                 search_range=None):
+        '''
+        Fit single or multiple components to an emission or absorption feature
+        Inputs:
+            line_wave: array like
+                wavelength in spectrum that corresponding to feature. Should be same length as line_flux
+            line_flux array like
+                flux in spectrum that corresponds to feature. Should be same length as line_wave
+            fit_wave: array like
+                wavelengths for the fit to be evaluated at (for plotting purposes)
+            fit_type: str (g, l, m)
+                type of function to fit: g=Gaussian, l=Lorentzian, m=Moffatt. Default is 'g'
+            center_list: list
+                list of (x, y) pair tuples corresponding to initial guesses for the position of 
+                the features being fit. The number of objects in this list defines the number
+                of lines fit.
+            ax1: matplotlib subplot object
+                subplot to plot the compound fit and its components
+            ax2: matplotlib subplot object
+                subplot to plot residuals of compound fit
+            continuum_l: endpoint named tuple
+                endpoint object containing the wavelength, flux, and error of the left continuum point 
+            continuum_r: endpoint named tuple
+                endpoint object containing the wavelength, flux, and error of the right continuum point
+            absorption (optional ): bool
+                if True (default) feature is treated as an absorption feature, 
+                False is not implemented
+            similar_widths: bool
+                if True (default) all lines fit are required to have the same width
+                            (either stddev or FWHM depending on the model)
+            fixed_offset: bool
+                If True, a fixed offset between gaussian means is used. offset keyword must also be defined.
+            offsets: array like
+                list of offsets of lines from left most line (e.g. for Ca II (8498, 8542, 8662), offsets = [44, 164])
+            search_range: int/None
+                If set to a number, the allowed values for the mean of each gaussian are bounded
+                by input mean (either from input_file or by clicking) +/- search_range
+        Outputs:
+            fit: astropy.modeling fit object
+                if return_fit is True, the astropy modeling fit object is returned.
+            lines: list
+                list of matplotlib line2d objects
+            args: dict
+                dictionary with keys 'size_arg', 'mean_arg', and 'width_arg' and values that correspond to the
+                parameter names in the model defined in fit_type
+            ax1: same axis object input - but with things plotted on it
+            ax2: same axis object input - but with things plotted on it
+              
+        '''
         if fit_type in PROFILES:
             profile, args = PROFILES[fit_type]
         else:
@@ -196,26 +244,59 @@ def fit_feature(line_wave, line_flux, fit_wave, fit_type, center_list, ax1, ax2,
 def define_feature(spectrum, line_name, absorption=True, 
                 similar_widths=True, fixed_offset = False, offsets=None,
                 input_filename=None, input_append=False, interactive=True,
-                return_fit=False, search_range=None):
+                return_fit=False, search_range=None, define_fit_range=True):
     '''
     Fit single or multiple components to an emission or absorption feature
     Inputs:
-        spectrum: spectrum1d class object with wave and flux attributes
-        absorption (optional ): if True (default) feature is treated as an absorption feature, 
-                        if False it is treated as an emission feature
-        similar_widths: if True (default) all lines fit are required to have the same width
+        spectrum: spectrum1d object
+            spectrum1d class object with wave and flux attributes
+        line_name: str
+            name of the line you are going to fit
+        absorption (optional ): bool
+            if True (default) feature is treated as an absorption feature, 
+            False is not implemented
+        similar_widths: bool
+            if True (default) all lines fit are required to have the same width
                         (either stddev or FWHM depending on the model)
+        fixed_offset: bool
+            If True, a fixed offset between gaussian means is used. offset keyword must also be defined.
+        offsets: array like
+            list of offsets of lines from left most line (e.g. for Ca II (8498, 8542, 8662), offsets = [44, 164])
+        input_filename: str
+            name of file that input fit parameters will be read from when interactive=False
+        input_append: bool
+            if True, input parameters will be appended to current informatio in input_filename, 
+            if False, the file will be over written
+        interactive: bool
+            If True, user is asked to define fitting parameters
+            If False, this information is read from input_filename file
+        return_fit: bool
+            If True, fit object is returned
+        search_range: int/None
+            If set to a number, the allowed values for the mean of each gaussian are bounded
+            by input mean (either from input_file or by clicking) +/- search_range
+        define_fit_range: bool
+            If True, user defines fit range and continuum separately
+            If False, user defined continuum and this is used as the fit range
     Outputs:
-        fit: an astropy compound model. The first component is always the continuum which
-             is set to 1.
-        Plot:
-            A plot is made of the continuum normalized flux, the region fit, the compound fit,
-            and the individual components of the fit
-        Print to screen:
-            Model used, center, FWHM, amplitude of each component
+        min_list: list
+            list of wavelength locations of minimum of each fit
+        pew_list: list
+            list of tuples (equivalent width, left error, right error) for each feature fit.
+            the error is defined as the value that includes 33.3% of the total integrated flux 
+            defined between continuum_l.wave and continuum_r.wave
+        fig: matplotlib figure object
+            a figure of the spectrum, including errors, the continuum points with errors, 
+            the fit bounding points, the minima wavelengths including errors, compound
+            fit, and the equivalent width (and error). This object can be saved if desired
+        fit: astropy.modeling fit object
+            if return_fit is True, the astropy modeling fit object is returned.
+        interactive_fig: matplotlib figure object
+            if return_fit is True, the figure of the compound fit and each individual element are returned
+
     
     Limitations:
-    * Fits either emission or absorption, not both
+    * Fit to emission is not yet implemented
     * If similar widths is used then all features have exactly the same width
     '''
     interactive_fig, ax1, ax2 = plot_spectrum(spectrum)  
@@ -228,10 +309,14 @@ def define_feature(spectrum, line_name, absorption=True,
     while True:
         #Select the region to fit
         fit_wave = np.arange(x_cont.min(), x_cont.max()+1, 0.01)
-        if interactive is True:
-            (x1, y1), (x2, y2) = id_fit_region_interactive()
+        if define_fit_range is False:
+            (x1,y1) = (continuum_l.wave, continuum_l.flux)
+            (x2, y2) = (continuum_r.wave, continuum_r.flux)
         else:
-            (x1, y1), (x2, y2) = get_id_fit_region_from_file(spectrum.filename, input_filename)
+            if interactive is True:
+                (x1, y1), (x2, y2) = id_fit_region_interactive()
+            else:
+                (x1, y1), (x2, y2) = get_id_fit_region_from_file(spectrum.filename, input_filename)
         #Select the normalized fit spectrum
         line_indx = (spectrum.wave<max(x1, x2)) & (spectrum.wave > min(x1, x2))
         line_wave = spectrum.wave[line_indx]
